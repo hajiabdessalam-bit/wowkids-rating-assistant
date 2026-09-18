49251cfff599232a539ad34913fb3a71c563a47e
hajiabdessalam-bit <haji.abdessalam@gmail.com>
2026-09-19 01:32:24 +0800

    Switch desktop launcher to selectable class mode

--- RUN_NEXT_STAGE.bat
+++ RUN_NEXT_STAGE.bat
@@ -3,14 +3,14 @@ setlocal enableextensions
 cd /d "%~dp0"
 
 echo ============================================================
-echo WOWKIDS HUMAN-LIKE ENGINE
+echo WOWKIDS CLASS RATING ASSISTANT
 echo ============================================================
 echo.
 echo Updating the project...
 git pull --ff-only
 if errorlevel 1 goto fail
 
-call RUN_HUMANLIKE_TEST.bat
+call RUN_CLASS.bat
 exit /b %ERRORLEVEL%
 
 :fail
